12dd1c4b7ac82911b7f13bda94eb76e3bcdb7ff8
DenisPepper <thech@yandex.ru>
2023-02-03 09:41:22 +0300

    added async loading promo product in App FC

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,10 @@
 import {AppRouter} from '../app-router/app-router';
+import {fetchPromoProduct} from '../../store/slices/promo/services/fetch-promo-product/fetch-promo-product';
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
 
 export default function App(): JSX.Element {
+  const dispatch = useAppDispatch();
+  dispatch(fetchPromoProduct());
+
   return <AppRouter/>;
 }
