66784691c800e9599fbb12e8a639a8b8328aeec9
DenisPepper <thech@yandex.ru>
2023-02-03 09:45:09 +0300

    added conditional render for Banner FC in catalog page

--- src/pages/catalog-page/catalog-page.tsx
+++ src/pages/catalog-page/catalog-page.tsx
@@ -1,12 +1,15 @@
 import {Outlet} from 'react-router-dom';
 import Banner from '../../components/banner/banner';
+import {useSelector} from 'react-redux';
+import {getPromoIsLoaded} from '../../store/slices/promo/selectors/get-promo-is-loaded/get-promo-is-loaded';
 
 export default function CatalogPage(): JSX.Element {
+  const isLoaded = useSelector(getPromoIsLoaded);
 
   return (
     <main>
-      <Banner/>
-      <Outlet context={1}/>
+      {isLoaded && <Banner/>}
+      <Outlet/>
     </main>
   );
 }
