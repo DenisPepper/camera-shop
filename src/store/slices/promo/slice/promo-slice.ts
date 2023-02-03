921dee22fc5bdc551125f6d942c2b0aae9496b9c
DenisPepper <thech@yandex.ru>
2023-02-03 09:38:16 +0300

    added redux slice for promo product

---
+++ src/store/slices/promo/slice/promo-slice.ts
@@ -0,0 +1,24 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {PromoSchema} from '../types/promo-schema';
+import {fetchPromoProduct} from '../services/fetch-promo-product/fetch-promo-product';
+
+const initialState: PromoSchema = {
+  product: null,
+  isLoaded: false,
+};
+
+export const promoSlice = createSlice({
+  name: 'promo',
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchPromoProduct.fulfilled, (state, action) => {
+        state.product = action.payload;
+        state.isLoaded = true;
+      });
+  }
+});
+
+export const { actions: promoActions } = promoSlice;
+export const { reducer: promoReducer } = promoSlice;
