5c133dc319fe9fe3017122e9b90bf57488074ec2
DenisPepper <thech@yandex.ru>
2023-02-03 10:34:01 +0300

    added fetching product by id on load product page

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -1,7 +1,12 @@
 import {useParams} from 'react-router-dom';
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import {fetchProductById} from '../../store/slices/product/services/fetch-product-by-id/fetch-product-by-id';
+
 
 export default function ProductPage(): JSX.Element {
   const {id = ''} = useParams();
+  const dispatch = useAppDispatch();
+  dispatch(fetchProductById({id}));
 
   return (<main>PRODUCT № {id} Page</main>);
 }
