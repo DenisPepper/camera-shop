1d068066783146dd778c9de4482feef2e963e525
DenisPepper <thech@yandex.ru>
2023-02-03 10:26:59 +0300

    added product type

---
+++ src/types/product-type.ts
@@ -0,0 +1,17 @@
+export interface ProductType {
+  id: 1;
+  name: string;
+  vendorCode: string;
+  type: string;
+  category: string;
+  description: string;
+  level: string;
+  rating: 4;
+  price: 73450;
+  previewImg: string;
+  previewImg2x: string;
+  previewImgWebp: string;
+  previewImgWebp2x: string;
+  reviewCount: 16;
+}
+
