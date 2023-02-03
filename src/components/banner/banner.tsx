8ca76044f1d994a8fe7c25a5af0c2cd4690f1b10
DenisPepper <thech@yandex.ru>
2023-02-03 09:44:07 +0300

    modified Banner FC

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,14 +1,23 @@
+import {useSelector} from 'react-redux';
+import {getPromoProductId} from '../../store/slices/promo/selectors/get-promo-product-id/get-promo-product-id';
+import {getPromoProductName} from '../../store/slices/promo/selectors/get-promo-product-name/get-promo-product-name';
+import {Link} from 'react-router-dom';
+import {RoutesConfig} from '../../settings/settings';
+
 export default function Banner(): JSX.Element {
+  const id = useSelector(getPromoProductId);
+  const name = useSelector(getPromoProductName);
+
   return (
     <div className={'banner'}>
       <picture>
         <source
           type="image/webp"
-          srcSet={'/img/content/banner-bg.webp, /img/content/banner-bg@2x.webp 2x'}
+          srcSet={'/img/content/promo.webp, /img/content/promo@2x.webp 2x'}
         />
         <img
-          src={'/img/content/banner-bg.jpg'}
-          srcSet={'/img/content/banner-bg@2x.jpg 2x'}
+          src={'/img/content/promo.jpg'}
+          srcSet={'/img/content/promo@2x.jpg 2x'}
           width="1280"
           height="280"
           alt="баннер"
@@ -16,9 +25,9 @@ export default function Banner(): JSX.Element {
       </picture>
       <p className="banner__info">
         <span className="banner__message">Новинка!</span>
-        <span className="title title--h1"> Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span>
+        {name && <span className="title title--h1">{name}</span>}
         <span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
-        <button className="btn" type='button'>Подробнее</button>
+        {id && <Link className={'btn'} to={`${RoutesConfig.Product.replace(/:id/, id.toString())}`}>Подробнее</Link>}
       </p>
     </div>
   );
