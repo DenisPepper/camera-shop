263d7880e2c2edeaca33554fc007ec520771a4c4
DenisPepper <thech@yandex.ru>
2023-02-05 11:01:53 +0300

    modified AppLayout FC

--- src/components/app-layout/app-layout.tsx
+++ src/components/app-layout/app-layout.tsx
@@ -1,15 +1,28 @@
-import {Outlet} from 'react-router-dom';
+import {Outlet, useLocation} from 'react-router-dom';
 import SvgSprite from '../svg-sprite/svg-sprite';
 import Header from '../header/header';
 import Footer from '../footer/footer';
+import {useSelector} from 'react-redux';
+import {getPromoIsLoaded} from '../../store/slices/promo/selectors/get-promo-is-loaded/get-promo-is-loaded';
+import Banner from '../banner/banner';
 
 export default function AppLayout(): JSX.Element {
+  const isLoaded = useSelector(getPromoIsLoaded);
+
+  const {pathname} = useLocation();
+  const isCatalogPage = /catalog/.test(pathname);
+
   return (
     <>
       <SvgSprite/>
       <div className={'wrapper'}>
         <Header/>
-        <Outlet/>
+        <main>
+          {isLoaded && isCatalogPage && <Banner/>}
+          <div className={'page-content'}>
+            <Outlet/>
+          </div>
+        </main>
         <Footer/>
       </div>
     </>
