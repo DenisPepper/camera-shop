732e37c5c75fd886501e3c970174b1c890ea21d9
DenisPepper <thech@yandex.ru>
2023-02-06 14:41:48 +0300

    added ProductCard FC

---
+++ src/components/product-card/product-card.tsx
@@ -0,0 +1,35 @@
+import {ProductType} from '../../types/product-type';
+import ProductCardInfo from '../product-card-info/product-card-info';
+import ProductCardImage from '../product-card-image/product-card-image';
+import ProductCardButton from '../product-card-button/product-card-button';
+
+interface ProductCardProps {
+  product: ProductType;
+}
+
+export default function ProductCard(props: ProductCardProps): JSX.Element {
+  const {product} = props;
+
+  return (
+    <div className={'product-card'}>
+      <ProductCardImage
+        key={'ProductCardImage'}
+        name={product.name}
+        previewImg={product.previewImg}
+        previewImg2x={product.previewImg2x}
+        previewImgWebp={product.previewImgWebp}
+        previewImgWebp2x={product.previewImgWebp2x}
+      />
+
+      <ProductCardInfo
+        key={'ProductCardInfo'}
+        name={product.name}
+        price={product.price}
+        rating={product.rating}
+        reviewCount={product.reviewCount}
+      />
+
+      <ProductCardButton key={'ProductCardButton'} id={product.id}/>
+    </div>
+  );
+}
