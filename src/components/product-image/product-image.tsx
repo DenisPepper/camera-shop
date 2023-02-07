33c9bd4a1e106aec81030dce4c4a711f39ce73e2
DenisPepper <thech@yandex.ru>
2023-02-07 17:48:32 +0300

    added ProductImage FC

---
+++ src/components/product-image/product-image.tsx
@@ -0,0 +1,27 @@
+import {ProductType} from '../../types/product-type';
+
+interface ProductImageProps {
+  product: ProductType;
+}
+
+export default function ProductImage(props: ProductImageProps): JSX.Element {
+  const {product: p} = props;
+
+  return (
+    <div className="product__img">
+      <picture>
+        <source
+          type="image/webp"
+          srcSet={`/${p.previewImgWebp}, /${p.previewImgWebp2x} 2x`}
+        />
+        <img
+          src={`/${p.previewImg}`}
+          srcSet={`/${p.previewImg2x} 2x`}
+          width="560"
+          height="480"
+          alt={p.name}
+        />
+      </picture>
+    </div>
+  );
+}
