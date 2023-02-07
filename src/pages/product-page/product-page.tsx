89a952395ce7e7f825df154b9dc873303fa185b0
DenisPepper <thech@yandex.ru>
2023-02-07 17:53:53 +0300

    added selector getProduct

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -1,18 +1,26 @@
-import {useParams} from 'react-router-dom';
-import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import Product from '../../components/product/product';
+import {useSelector} from 'react-redux';
 import {fetchProductById} from '../../store/slices/product/services/fetch-product-by-id/fetch-product-by-id';
-
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import {useParams} from 'react-router-dom';
+import {useEffect} from 'react';
+import {getProduct} from '../../store/slices/product/selectors/get-product/get-product';
 
 export default function ProductPage(): JSX.Element {
   const {id = ''} = useParams();
   const dispatch = useAppDispatch();
-  dispatch(fetchProductById({id}));
+  const product = useSelector(getProduct);
 
-  return (
+  useEffect(() => {
+    dispatch(fetchProductById({id}));
+  }, [id, dispatch]);
+
+  return !!product && product?.id.toString() === id ?
     <>
-      <div className={'page-content__section'}></div>
+      <Product key={'Product'} product={product}/>
       <div className={'page-content__section'}></div>
       <div className={'page-content__section'}></div>
     </>
-  );
+    :
+    <div className={'page-content__section'}></div>;
 }
