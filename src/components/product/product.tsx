893a18d7dbe75bb3d131929fc2349f9df111e66d
DenisPepper <thech@yandex.ru>
2023-02-09 11:09:32 +0300

    fixed Product FC

--- src/components/product/product.tsx
+++ src/components/product/product.tsx
@@ -1,21 +1,21 @@
 import ProductImage from '../product-image/product-image';
 import ProductContent from '../product-content/product-content';
-import {ProductType} from '../../types/product-type';
+import {getProduct} from '../../store/slices/product/selectors/get-product/get-product';
+import {useSelector} from 'react-redux';
 
-interface ProductProps {
-  product: ProductType;
-}
-
-export default function Product(props: ProductProps): JSX.Element {
-  const {product} = props;
+export default function Product(): JSX.Element {
+  const product = useSelector(getProduct);
 
   return (
     <div className={'page-content__section'}>
-      <section className={'product'}>
-        <div className={'container'}>
-          <ProductImage key={'ProductImage'} product={product}/>
-          <ProductContent key={'ProductContent'} product={product}/>
-        </div>
-      </section>
+      {
+        !!product &&
+        <section className={'product'}>
+          <div className={'container'}>
+            <ProductImage key={'ProductImage'} product={product}/>
+            <ProductContent key={'ProductContent'} product={product}/>
+          </div>
+        </section>
+      }
     </div>);
 }
