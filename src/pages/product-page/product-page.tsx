c01c2337d75d1cccbc80c01999fb76f1e3e0b4fe
DenisPepper <thech@yandex.ru>
2023-02-09 11:09:12 +0300

    fixed ProductPage

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -4,20 +4,22 @@ import {fetchProductById} from '../../store/slices/product/services/fetch-produc
 import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
 import {useParams} from 'react-router-dom';
 import {useEffect} from 'react';
-import {getProduct} from '../../store/slices/product/selectors/get-product/get-product';
+import {getProductId} from '../../store/slices/product/selectors/get-product-id/get-product-id';
 
 export default function ProductPage(): JSX.Element {
   const {id = ''} = useParams();
   const dispatch = useAppDispatch();
-  const product = useSelector(getProduct, shallowEqual);
+  const lastLoadedID = useSelector(getProductId, shallowEqual)?.toString() || '';
 
   useEffect(() => {
-    dispatch(fetchProductById({id}));
-  }, [id, dispatch]);
+    if (lastLoadedID !== id) {
+      dispatch(fetchProductById({id}));
+    }
+  }, [id, lastLoadedID, dispatch]);
 
-  return !!product && product?.id.toString() === id ?
+  return lastLoadedID === id ?
     <>
-      <Product key={'Product'} product={product}/>
+      <Product key={'Product'}/>
       <div className={'page-content__section'}></div>
       <div className={'page-content__section'}></div>
     </>
