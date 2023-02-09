38480138c9500ae36a1f6dde8dcd2d03646c3cc4
DenisPepper <thech@yandex.ru>
2023-02-09 11:07:57 +0300

    fixed BreadcrumbsItemWithProductName FC

--- src/components/breadcrumbs-item-with-product-name/breadcrumbs-item-with-product-name.tsx
+++ src/components/breadcrumbs-item-with-product-name/breadcrumbs-item-with-product-name.tsx
@@ -1,17 +1,18 @@
 import {shallowEqual, useSelector} from 'react-redux';
 import {getProductName} from '../../store/slices/product/selectors/get-product-name/get-product-name';
-
-/*interface BreadcrumbsItemProps {
-  id: string;
-}*/
+import {useParams} from 'react-router-dom';
+import {getProductId} from '../../store/slices/product/selectors/get-product-id/get-product-id';
 
 export default function BreadcrumbsItemWithProductName(): JSX.Element {
+  const {id} = useParams() || '';
+
   const name = useSelector(getProductName, shallowEqual);
+  const lastLoadedID = useSelector(getProductId, shallowEqual)?.toString() || '';
 
   return (
     <li className="breadcrumbs__item">
       <span className="breadcrumbs__link breadcrumbs__link--active">
-        {name}
+        {id === lastLoadedID ? name : ''}
       </span>
     </li>
   );
