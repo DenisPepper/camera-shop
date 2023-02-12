3303c78a419a8e871044fd9fe7a6247634cbb1ff
DenisPepper <thech@yandex.ru>
2023-02-12 23:19:02 +0300

    modified AppPopup FC

--- src/components/app-popup/app-popup.tsx
+++ src/components/app-popup/app-popup.tsx
@@ -1,20 +1,40 @@
 import AppPortal from '../app-portal/app-portal';
 import {ROOT} from '../../index';
-import {ReactNode} from 'react';
+import React, {ReactNode, useCallback, useEffect} from 'react';
 
 interface AppPopupProps {
   children?: ReactNode;
   isOpen: boolean;
+  overlayOnClickHandler: () => void;
+  onEscapeKeyDownHandler: () => void;
 }
 
 export default function AppPopup(props: AppPopupProps): JSX.Element {
-  const {children, isOpen} = props;
+  const {
+    children,
+    isOpen,
+    overlayOnClickHandler,
+    onEscapeKeyDownHandler
+  } = props;
+
+  const handleOnKeyDown = useCallback((evt: KeyboardEvent) => {
+    if (evt.key === 'Escape') {
+      onEscapeKeyDownHandler();
+    }
+  }, [onEscapeKeyDownHandler]);
+
+  useEffect(() => {
+    if (isOpen) {
+      window.addEventListener('keydown', handleOnKeyDown);
+    }
+    return () => window.removeEventListener('keydown', handleOnKeyDown);
+  }, [isOpen, handleOnKeyDown]);
 
   return (
     <AppPortal container={ROOT}>
       <div className={`modal ${isOpen ? 'is-active' : ''}`}>
         <div className={'modal__wrapper'}>
-          <div className="modal__overlay"></div>
+          <div className="modal__overlay" onClick={overlayOnClickHandler}></div>
           <div className={'modal__content'}>
             {children}
           </div>
