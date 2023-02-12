36f51a8066e9c29d933c48d2e0f2366f93d5fa8d
DenisPepper <thech@yandex.ru>
2023-02-12 23:19:25 +0300

    modified PostReviewPopupProps FC

--- src/components/post-review-popup/post-review-popup.tsx
+++ src/components/post-review-popup/post-review-popup.tsx
@@ -1,15 +1,37 @@
 import AppPopup from '../app-popup/app-popup';
+import {shallowEqual, useSelector} from 'react-redux';
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import {reviewPopupActions} from '../../store/slices/review-popup/slice/review-popup-slice';
+import {
+  getReviewPopupIsOpen
+} from '../../store/slices/review-popup/selectors/get-review-popup-is-open/get-review-popup-is-open';
 
-interface PostReviewPopupProps {
-  isOpen: boolean;
-}
+export default function PostReviewPopup(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const isMounted = useSelector(getReviewPopupIsOpen, shallowEqual);
 
-export default function PostReviewPopup(props: PostReviewPopupProps): JSX.Element {
-  const {isOpen} = props;
+  const handleOnCloseClick = () => {
+    dispatch(reviewPopupActions.close());
+  };
 
   return (
-    <AppPopup isOpen={isOpen}>
-      Форма отправки отзыва
+    <AppPopup
+      isOpen={isMounted}
+      overlayOnClickHandler={handleOnCloseClick}
+      onEscapeKeyDownHandler={handleOnCloseClick}
+    >
+      <p className="title title--h4">Оставить отзыв</p>
+
+      <button
+        className="cross-btn"
+        type="button"
+        aria-label="Закрыть попап"
+        onClick={handleOnCloseClick}
+      >
+        <svg width="10" height="10" aria-hidden="true">
+          <use xlinkHref="#icon-close"></use>
+        </svg>
+      </button>
     </AppPopup>
   );
 }
