47afc36d40153690300d064ff90e12ef3a37b5ae
DenisPepper <thech@yandex.ru>
2023-02-15 14:06:23 +0300

    removed styles from index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import StoreProvider from './store/store-provider';
 import {BrowserRouter} from 'react-router-dom';
 import App from './components/app/app';
-import './index.css';
 
 export const ROOT = document.getElementById('root') as HTMLElement;
 
