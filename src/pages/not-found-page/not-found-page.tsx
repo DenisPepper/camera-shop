9f1e98585fe62844fa828bcd01b89caa3550d290
DenisPepper <thech@yandex.ru>
2023-02-15 14:03:43 +0300

    modified and styled NotFoundPage FC

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,3 +1,14 @@
+import './not-found-page.css';
+import {Link} from 'react-router-dom';
+import {Path} from '../../settings/settings';
+
 export default function NotFoundPage(): JSX.Element {
-  return (<main>404. Page NOT FOUND</main>);
+  return (
+    <div className={'wrapper--not-found'}>
+      <h1 className="title title--h1">404 Not Found</h1>
+      <Link className={'main-nav__link'} to={Path.Main}>
+        Перейти на главную страницу
+      </Link>
+    </div>
+  );
 }
