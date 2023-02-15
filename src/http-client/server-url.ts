4deccbcf05b52e9abf10425bf21ada02de238321
DenisPepper <thech@yandex.ru>
2023-02-15 14:11:16 +0300

    expanded ServerUrl enum

--- src/http-client/server-url.ts
+++ src/http-client/server-url.ts
@@ -3,6 +3,7 @@ import {MAX_PRODUCT_COUNT} from '../settings/settings';
 export const enum ServerUrl {
   PromoProduct = 'https://camera-shop.accelerator.pages.academy/promo',
   Product = 'https://camera-shop.accelerator.pages.academy/cameras/',
+  PostReview = 'https://camera-shop.accelerator.pages.academy/reviews/'
 }
 
 export const PRODUCTS_URL = 'https://camera-shop.accelerator.pages.academy/cameras';
