3abe3fe0bc646e2838e10853d7e1709967b20395
DenisPepper <thech@yandex.ru>
2023-02-16 18:53:03 +0300

    minor fixes

--- src/store/slices/review/selectors/get-review-list/get-review-list.ts
+++ src/store/slices/review/selectors/get-review-list/get-review-list.ts
@@ -3,6 +3,5 @@ import {getReviewSlice} from '../get-review-slice/get-review-slice';
 import {ReviewSchema} from '../../schema/review-schema';
 
 
-export const getReviewList = createSelector(
-  getReviewSlice,
+export const getReviewList = createSelector(getReviewSlice,
   (review: ReviewSchema) => review.list);
