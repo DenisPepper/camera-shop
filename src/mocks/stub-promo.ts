86730e12ab76e6c9a8b3fb368070bdb1b656b4af
DenisPepper <thech@yandex.ru>
2023-02-21 14:36:52 +0300

    added tests on promo-slice extra reducers

---
+++ src/mocks/stub-promo.ts
@@ -0,0 +1,8 @@
+export const stubPromo = {
+  id: 0,
+  name: 'string',
+  previewImg: 'string',
+  previewImg2x: 'string',
+  previewImgWebp: 'string',
+  previewImgWebp2x: 'string',
+};
---
+++ src/store/slices/promo/slice/promo-slice.test.ts
@@ -0,0 +1,35 @@
+import {PromoSchema as Schema} from '../schema/promo-schema';
+import {promoReducer as reducer} from './promo-slice';
+import {stubPromo} from '../../../../mocks/stub-promo';
+import {fetchPromoProduct} from '../services/fetch-promo-product/fetch-promo-product';
+
+describe('test of promo-slice reducer', () => {
+
+  it('should return initial state', () => {
+    const initialState: Schema = {
+      product: null,
+      isLoaded: false,
+    };
+
+    expect(reducer(undefined, {type: undefined}))
+      .toEqual(initialState);
+  });
+
+  it('should return promo product on fetchPromoProduct fulfilled', () => {
+    const prevState: Schema = {
+      product: null,
+      isLoaded: false,
+    };
+    const updatedState: Schema = {
+      product: stubPromo,
+      isLoaded: true,
+    };
+
+    expect(reducer(prevState,
+      {
+        type: fetchPromoProduct.fulfilled.type,
+        payload: stubPromo,
+      }
+    )).toEqual(updatedState);
+  });
+});
