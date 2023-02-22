854dd8f90dcc9ee3327119e9c9d2d1409a8cd767
DenisPepper <thech@yandex.ru>
2023-02-22 14:09:32 +0300

    added test draft for ProductPage FC

---
+++ src/pages/product-page/product-page.test.tsx
@@ -0,0 +1,8 @@
+import ProductPage from './product-page';
+import {render} from '@testing-library/react';
+import {BrowserRouter} from 'react-router-dom';
+
+it.todo('should render ProductPage FC', () => {
+  render(<ProductPage/>, {wrapper: BrowserRouter});
+  // TODO write test
+});
