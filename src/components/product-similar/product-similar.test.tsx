2ef24a9fbfe1477ceb2bc49cad665dbf50d4198d
DenisPepper <thech@yandex.ru>
2023-02-22 20:29:48 +0300

    added test for ProductSimilar FC

---
+++ src/components/product-similar/product-similar.test.tsx
@@ -0,0 +1,37 @@
+import ProductSimilar from './product-similar';
+import {render, screen} from '@testing-library/react';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {Provider} from 'react-redux';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
+import {stubProduct} from '../../mocks/stub-product';
+import {BrowserRouter} from 'react-router-dom';
+
+describe('test ProductSimilar', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+
+  it('should render with expected text', () => {
+    const initialState: DeepPartial<StateSchema> = {
+      similar: {products: [stubProduct]}
+    };
+    const store = mockStore(initialState);
+
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <ProductSimilar/>
+        </BrowserRouter>
+      </Provider>);
+
+    const pageTitle = screen.getByText(/Похожие товары/);
+    expect(pageTitle).toBeInTheDocument();
+  });
+
+});
+
+
