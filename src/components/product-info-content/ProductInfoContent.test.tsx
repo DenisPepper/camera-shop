f7b0e2b0576399b023cb1ae3de0dfed8f7d11a81
DenisPepper <thech@yandex.ru>
2023-02-23 22:03:03 +0300

    added test ProductInfoContent FC

---
+++ src/components/product-info-content/ProductInfoContent.test.tsx
@@ -0,0 +1,19 @@
+import ProductInfoContent from './product-info-content';
+import {render, screen} from '@testing-library/react';
+import {stubProduct} from '../../mocks/stub-product';
+import {ProductTab as Tab} from '../../settings/settings';
+
+describe('test ProductInfoContent FC', () => {
+  const stubCallback = jest.fn();
+
+  it('should be render without crushing', () => {
+    render(
+      <ProductInfoContent
+        product={stubProduct}
+        tab={Tab.Characteristic}
+        onTabClickHandler={stubCallback}
+      />);
+    const element = screen.getByText('Добавить в корзину');
+    expect(element).toBeInTheDocument();
+  });
+});
