97b77e774f880d20aeb0f546f29111a9e95af9fb
DenisPepper <thech@yandex.ru>
2023-02-23 11:14:18 +0300

    added test for ProductReviewHeader FC

---
+++ src/components/product-review-header/product-review-header.test.tsx
@@ -0,0 +1,22 @@
+import ProductReviewHeader from './product-review-header';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('test ProductReviewHeader FC', () => {
+
+  const stubCallback = jest.fn();
+
+  it('should render without crushing', () => {
+    render(<ProductReviewHeader onClickPostReview={stubCallback}/>);
+    const element = screen.getByText(/Оставить свой отзыв/);
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should run callback function', async () => {
+    render(<ProductReviewHeader onClickPostReview={stubCallback}/>);
+    const element = screen.getByText(/Оставить свой отзыв/);
+    await userEvent.click(element);
+    expect(stubCallback).toHaveBeenCalledTimes(1);
+  });
+
+});
