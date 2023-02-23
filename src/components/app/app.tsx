b7d974a1c439fde4993d0b5636d1dca859239a98
DenisPepper <thech@yandex.ru>
2023-02-23 12:07:26 +0300

    fixed App FC

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,19 +1,12 @@
 import {AppRouter} from '../app-router/app-router';
 import {fetchPromoProduct} from '../../store/slices/promo/services/fetch-promo-product/fetch-promo-product';
 import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
-import {BrowserRouter} from 'react-router-dom';
-import StoreProvider from '../../store/store-provider';
-import React from 'react';
 
 export function App(): JSX.Element {
   const dispatch = useAppDispatch();
   dispatch(fetchPromoProduct());
 
   return (
-    <StoreProvider>
-      <BrowserRouter>
-        <AppRouter/>
-      </BrowserRouter>
-    </StoreProvider>
+    <AppRouter/>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {App} from './components/app/app';
+import StoreProvider from './store/store-provider';
+import {BrowserRouter} from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <React.StrictMode>
-    <App/>
+    <StoreProvider>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
+    </StoreProvider>
   </React.StrictMode>,
 );
