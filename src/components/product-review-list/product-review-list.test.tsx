f02c5020d4b02a89f873704b177903e6f7de2722
DenisPepper <thech@yandex.ru>
2023-02-23 10:32:36 +0300

    fixed test for ProductReviewPopup FC

--- src/components/product-review-list/product-review-list.test.tsx
+++ src/components/product-review-list/product-review-list.test.tsx
@@ -5,16 +5,12 @@ import { logRoles } from '@testing-library/react';
 
 describe('test ProductReviewList FC', () => {
 
-  it('should render without crushing with empty array', () => {
-    render(<ProductReviewList reviews={[]}/>);
-  });
-
   it('should render without crushing with filled array', () => {
     render(<ProductReviewList reviews={[stubReview]}/>);
   });
 
-  it('should render ul list with expected css class', () => {
-    const { container } = render(<ProductReviewList reviews={[stubReview]}/>);
+  it('should render empty ul list with expected css class', () => {
+    const { container } = render(<ProductReviewList reviews={[]}/>);
     logRoles(container);
     const element = screen.getByRole('list');
     expect(element).toHaveClass('review-block__list');
