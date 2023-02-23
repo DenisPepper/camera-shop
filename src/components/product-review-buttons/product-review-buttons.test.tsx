a644ee22dc9ac68f0705918f57f6e0deb7fe2581
DenisPepper <thech@yandex.ru>
2023-02-23 14:30:57 +0300

    added test for ProductReviewButtons FC

---
+++ src/components/product-review-buttons/product-review-buttons.test.tsx
@@ -0,0 +1,40 @@
+import ProductReviewButtons from './product-review-buttons';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+describe('test ProductReviewButtons FC', () => {
+  const stubCallback = jest.fn();
+
+  it('should render without crushing', () => {
+    render(
+      <ProductReviewButtons
+        onClickShowMore={stubCallback}
+        shouldHide
+      />);
+    const element = screen.getByText(/Показать больше отзывов/);
+    expect(element).toBeInTheDocument();
+  });
+
+  it('should run callback function', async () => {
+    render(
+      <ProductReviewButtons
+        onClickShowMore={stubCallback}
+        shouldHide
+      />);
+    const element = screen.getByRole('button');
+    await userEvent.click(element);
+    expect(stubCallback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should render with expected scc class', () => {
+    render(
+      <ProductReviewButtons
+        onClickShowMore={stubCallback}
+        shouldHide
+      />);
+    const element = screen.getByTestId('button-container');
+    expect(element).toBeInTheDocument();
+    expect(element).toHaveClass('visually-hidden');
+  });
+
+});
