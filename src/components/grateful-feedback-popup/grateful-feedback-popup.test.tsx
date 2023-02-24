9cd99fbd1c6f7f2aa6c81a50e4d7c07f07f8c0f1
DenisPepper <thech@yandex.ru>
2023-02-24 17:47:14 +0300

    added test for GratefulFeedbackPopup FC

---
+++ src/components/grateful-feedback-popup/grateful-feedback-popup.test.tsx
@@ -0,0 +1,28 @@
+import GratefulFeedbackPopup from './grateful-feedback-popup';
+import {render} from '@testing-library/react';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+
+describe('test GratefulFeedbackPopup FC', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+  const initialState: DeepPartial<StateSchema> = {
+    gratefulFeedbackPopup: {isOpen: false}
+  };
+  const store = mockStore(initialState);
+
+  it('should render without fail', () => {
+
+    render(
+      <Provider store={store}>
+        <GratefulFeedbackPopup/>
+      </Provider>
+    );
+  });
+});
