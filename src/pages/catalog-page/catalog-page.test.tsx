5b89619810d3d8188e495e2f63b5b70424587f1f
DenisPepper <thech@yandex.ru>
2023-02-25 10:23:55 +0300

    modified test for CatalogPage FC

--- src/pages/catalog-page/catalog-page.test.tsx
+++ src/pages/catalog-page/catalog-page.test.tsx
@@ -1,7 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import {render} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
 import CatalogPage from './catalog-page';
 
-it.todo('should render CatalogPage FC');
+
+describe('test CatalogPage FC', () => {
+
+  it('should render without fail', () => {
+    render(<CatalogPage/>,
+      {wrapper: BrowserRouter});
+  });
+});
