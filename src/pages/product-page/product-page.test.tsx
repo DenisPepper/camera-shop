bb213384604c5c9f361cfdfb5912bed9527ca59c
DenisPepper <thech@yandex.ru>
2023-02-25 10:20:16 +0300

    added test for ProductPage FC

--- src/pages/product-page/product-page.test.tsx
+++ src/pages/product-page/product-page.test.tsx
@@ -1,7 +1,20 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import ProductPage from './product-page';
 import {render} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
 
-it.todo('should render ProductPage FC');
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn(),
+}));
+
+it('should render ProductPage FC without fail', () => {
+
+  render(<ProductPage/>,
+    {wrapper: BrowserRouter});
+});
