e518d2cf6aa8bf61090f9ce3e362624c92b05e1b
DenisPepper <thech@yandex.ru>
2023-02-25 10:02:47 +0300

    added test for AppPopupCloseButton FC

---
+++ src/components/app-popup-close-button/app-popup-close-button.test.tsx
@@ -0,0 +1,21 @@
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import AppPopupCloseButton from './app-popup-close-button';
+import {BrowserRouter} from 'react-router-dom';
+
+
+describe('test AppPopupCloseButton FC', () => {
+
+  const onClick = jest.fn();
+  const onFocus = jest.fn();
+
+  it('should render without fail', async () => {
+    render(
+      <AppPopupCloseButton handleOnClick={onClick} handleOnFocus={onFocus}/>,
+      {wrapper: BrowserRouter}
+    );
+    const element = await screen.findByRole('button');
+    await userEvent.click(element);
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+});
