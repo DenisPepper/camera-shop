289f8c32c78c692218ac7fb25f1bde795fef885b
DenisPepper <thech@yandex.ru>
2023-02-25 10:22:07 +0300

    added test for MainPage FC

--- src/pages/main-page/main-page.test.tsx
+++ src/pages/main-page/main-page.test.tsx
@@ -1,7 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import {render} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
 import MainPage from './main-page';
 
-it.todo('should render MainPage FC', );
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: jest.fn(),
+}));
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useEffect: jest.fn(),
+}));
+
+it('should render MainPage FC', () => {
+  render(<MainPage/>,
+    {wrapper: BrowserRouter});
+});
