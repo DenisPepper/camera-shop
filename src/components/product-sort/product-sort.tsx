1f3c32fc576624d051f5ae9edc4b8f69e57501f5
DenisPepper <thech@yandex.ru>
2023-03-08 21:12:58 +0300

    modified ProductSort FC

--- src/components/product-sort/product-sort.tsx
+++ src/components/product-sort/product-sort.tsx
@@ -1,5 +1,19 @@
+import ProductSortTarget from '../product-sort-type/product-sort-target';
+import ProductSortOrder from '../product-sort-order/product-sort-order';
+
 export default function ProductSort(): JSX.Element {
   return (
-    <div className={'catalog-sort'}>Сортировки</div>
+    <div className={'catalog-sort'}>
+      <form action="#">
+        <div className="catalog-sort__inner">
+          <p className="title title--h5">Сортировать:</p>
+
+          <ProductSortTarget />
+
+          <ProductSortOrder />
+
+        </div>
+      </form>
+    </div>
   );
 }
