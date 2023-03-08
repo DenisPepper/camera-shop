bf0a3bad46f592d1b0e325f655a43fb4c06a8931
DenisPepper <thech@yandex.ru>
2023-03-08 08:11:25 +0300

    modified HeaderSearchForm FC

--- src/components/header-search-form/header-search-form.tsx
+++ src/components/header-search-form/header-search-form.tsx
@@ -1,27 +1,61 @@
+import HeaderSearchResetButton from '../header-search-reset-button/header-search-reset-button';
+import HeaderSearchInput from '../header-search-input/header-search-input';
+import HeaderSearchList from '../header-search-list/header-search-list';
+import {useRef, useState} from 'react';
+import {
+  fetchProductsByNameSubstring
+} from './services/fetch-products-by-name-substring/fetch-products-by-name-substring';
+import {ProductType} from '../../types/product-type';
+import {DEFAULT_PRODUCT_TAB, Path as to} from '../../settings/settings';
+import {useNavigate} from 'react-router-dom';
+
 export default function HeaderSearchForm(): JSX.Element {
+  const navigate = useNavigate();
+  const [notEmpty, setNotEmpty] = useState(() => false);
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const formRef = useRef<HTMLFormElement | null>(null);
+
+  const handleFormInput = (value: string) => {
+    setNotEmpty(() => !!value);
+    value ?
+      fetchProductsByNameSubstring(value)
+        .then((data) => setProducts(data))
+      :
+      setProducts([]);
+  };
+
+  const handleFormPick = (id: number) => {
+    formRef.current?.reset();
+    navigate(`/${to.Product}/${id}?tab=${DEFAULT_PRODUCT_TAB}`);
+  };
+
+  const handleFormReset = () => {
+    setNotEmpty(() => false);
+    setProducts(() => []);
+  };
+
   return (
-    <div className="form-search">
-      <form>
-        <label>
-          <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
-            <use xlinkHref="#icon-lens"></use>
-          </svg>
-          <input
-            className="form-search__input"
-            type="text"
-            autoComplete="off"
-            placeholder="Поиск по сайту"
-          />
-        </label>
-        <ul className="form-search__select-list">
-        </ul>
+    <div className={`form-search ${products.length ? 'list-opened' : ''}`}>
+      <form onReset={handleFormReset} ref={formRef}>
+
+        <HeaderSearchInput
+          key={'HeaderSearchInput'}
+          handleFormInput={handleFormInput}
+        />
+
+        {!!products.length &&
+          <HeaderSearchList
+            key={'HeaderSearchList'}
+            products={products}
+            handleFormPick={handleFormPick}
+          />}
+
+        {notEmpty &&
+          <HeaderSearchResetButton
+            key={'HeaderSearchResetButton'}
+          />}
       </form>
-      <button className="form-search__reset" type="reset">
-        <svg width="10" height="10" aria-hidden="true">
-          <use xlinkHref="#icon-close"></use>
-        </svg>
-        <span className="visually-hidden">Сбросить поиск</span>
-      </button>
+
     </div>
   );
 }
