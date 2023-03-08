2e7de83dd68d97715199ee37e376df9cce963923
DenisPepper <thech@yandex.ru>
2023-03-08 21:11:51 +0300

    added ProductSortTarget FC

---
+++ src/components/product-sort-type/product-sort-target.tsx
@@ -0,0 +1,28 @@
+import {SortType} from '../../types/sort-types';
+import ProductSortTypeInput from '../product-sort-type-input/product-sort-type-input';
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import {sortActions} from '../../store/slices/sort/slice/sort-slice';
+
+export default function ProductSortTarget(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const handleSortTargetClick = (sort: SortType) => {
+    dispatch(sortActions.setSort(sort));
+  };
+
+  return (
+    <div className="catalog-sort__type">
+
+      <ProductSortTypeInput
+        sort={'price'}
+        handleSortTargetClick={handleSortTargetClick}
+      />
+
+      <ProductSortTypeInput
+        sort={'rating'}
+        handleSortTargetClick={handleSortTargetClick}
+      />
+
+    </div>
+  );
+}
