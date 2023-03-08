ec0c6a4a5aa778a92c8ad36f0f72f7b1ed07e5f1
DenisPepper <thech@yandex.ru>
2023-03-08 08:07:57 +0300

    added debounce func for search input

---
+++ src/lib/debounce-search-input/debounce-search-input.ts
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const DEFAULT_DELAY = 500;
+
+type Event = React.ChangeEvent<HTMLInputElement>;
+type Callback = (evt: Event) => void;
+type TimeoutID = ReturnType<typeof setTimeout>;
+
+export const debounceSearchInput = (callback: Callback, delay = DEFAULT_DELAY) => {
+  let timeoutID: TimeoutID;
+
+  return (evt: Event) => {
+    clearTimeout(timeoutID);
+    timeoutID = setTimeout(() => callback(evt), delay);
+  };
+};
