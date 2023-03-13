18b4267fbc67629d7f9c8f991b4032548fdf78aa
DenisPepper <thech@yandex.ru>
2023-03-13 08:04:32 +0300

    modified FilterPrice FC

--- src/components/filter-price/filter-price.tsx
+++ src/components/filter-price/filter-price.tsx
@@ -1,4 +1,4 @@
-import React, {useLayoutEffect, useRef, useState} from 'react';
+import React, {useLayoutEffect, useState} from 'react';
 import {FilterPriceParams} from '../../settings/settings';
 import {fetchMinMaxPrice} from '../../services/fetch-min-max-price/fetch-min-max-price';
 import {useSearchParams} from 'react-router-dom';
@@ -19,8 +19,6 @@ export default function FilterPrice(): JSX.Element {
   const [maxCatalogPrice, setMaxCatalogPrice] = useState<string>('');
   const [minModifier, setMinModifier] = useState('');
   const [maxModifier, setMaxModifier] = useState('');
-  const minRef = useRef<HTMLInputElement | null>(null);
-  const maxRef = useRef<HTMLInputElement | null>(null);
 
   const handleMinPriceBlur = (evt: React.FocusEvent<HTMLInputElement>) => {
     const value = Number(evt.target.value);
@@ -136,7 +134,6 @@ export default function FilterPrice(): JSX.Element {
         <div className={`custom-input ${minModifier}`}>
           <label>
             <input
-              ref={minRef}
               type="number"
               name="price"
               placeholder={minCatalogPrice}
@@ -149,7 +146,6 @@ export default function FilterPrice(): JSX.Element {
         <div className={`custom-input ${maxModifier}`}>
           <label>
             <input
-              ref={maxRef}
               type="number"
               name="priceUp"
               placeholder={maxCatalogPrice}
