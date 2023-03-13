8cf9f4f4deea5ef97556de3a2190fa9c66021b10
DenisPepper <thech@yandex.ru>
2023-03-13 15:53:09 +0300

    added filters types

---
+++ src/types/filter-types.ts
@@ -0,0 +1,6 @@
+export type ProductCategory = 'photocamera' | 'videocamera';
+
+export type ProductGroup = 'digital' | 'film' | 'snapshot' | 'collection';
+
+export type ProductLevel = 'zero' | 'non-professional' | 'professional';
+
