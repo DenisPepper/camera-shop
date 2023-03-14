a2021783e3f7513e2883afb77cbfbc8daa403e14
DenisPepper <thech@yandex.ru>
2023-03-14 15:13:41 +0300

    extracted from url and wrote search params to search-params reducer

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,21 @@
 import {AppRouter} from '../app-router/app-router';
+import {useLayoutEffect} from 'react';
+import {useAppDispatch} from '../../hooks/use-app-dispatch.ts/use-app-dispatch';
+import {useSearchParams} from 'react-router-dom';
+import {SortDirectionType, SortType} from '../../types/sort-types';
+import {searchParamsActions} from '../../store/slices/search-params/slice/search-params-slice';
 
 export function App(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const [searchParams] = useSearchParams();
+  const sort: SortType = searchParams.get('sort') as SortType || '';
+  const order: SortDirectionType = searchParams.get('order') as SortDirectionType || '';
+
+  useLayoutEffect(() => {
+    dispatch(searchParamsActions.setSort(sort));
+    dispatch(searchParamsActions.setOrder(order));
+
+  }, []);
 
   return (
     <AppRouter/>
