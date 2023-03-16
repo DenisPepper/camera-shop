3118065bf7822c7afc3edda1146e5dec176a20a8
DenisPepper <thech@yandex.ru>
2023-03-16 19:21:11 +0300

    modified errorSlice reducer

--- src/store/slices/error/slice/error-slice.ts
+++ src/store/slices/error/slice/error-slice.ts
@@ -3,6 +3,10 @@ import {ErrorSchema} from '../schema/error-schema';
 import {
   fetchProductByIdWithReviews
 } from '../../../../services/fetch-product-by-id-with-reviews/fetch-product-by-id-with-reviews';
+import {fetchProducts} from '../../../../services/fetch-products/fetch-products';
+import {fetchPromoProduct} from '../../../../services/fetch-promo-product/fetch-promo-product';
+import {fetchSimilar} from '../../../../services/fetch-similar/fetch-similar';
+import {postReview} from '../../../../services/post-review/post-review';
 
 const initialState: ErrorSchema = {
   errors: [],
@@ -23,6 +27,30 @@ export const errorSlice = createSlice({
   extraReducers: (builder) => {
     builder
       .addCase(fetchProductByIdWithReviews.rejected, (state, action) => {
+        if (action.payload) {
+          const errorsWithoutNew = state.errors.filter((err) => err !== action.payload);
+          state.errors = [...errorsWithoutNew, action.payload];
+        }
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        if (action.payload) {
+          const errorsWithoutNew = state.errors.filter((err) => err !== action.payload);
+          state.errors = [...errorsWithoutNew, action.payload];
+        }
+      })
+      .addCase(fetchPromoProduct.rejected, (state, action) => {
+        if (action.payload) {
+          const errorsWithoutNew = state.errors.filter((err) => err !== action.payload);
+          state.errors = [...errorsWithoutNew, action.payload];
+        }
+      })
+      .addCase(fetchSimilar.rejected, (state, action) => {
+        if (action.payload) {
+          const errorsWithoutNew = state.errors.filter((err) => err !== action.payload);
+          state.errors = [...errorsWithoutNew, action.payload];
+        }
+      })
+      .addCase(postReview.rejected, (state, action) => {
         if (action.payload) {
           const errorsWithoutNew = state.errors.filter((err) => err !== action.payload);
           state.errors = [...errorsWithoutNew, action.payload];
