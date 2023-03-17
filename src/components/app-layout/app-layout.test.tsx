fb7f15490b15638474b536611f545f830f974601
DenisPepper <thech@yandex.ru>
2023-03-17 17:39:37 +0300

    fixed tests for AppLayout FC

--- src/components/app-layout/app-layout.test.tsx
+++ src/components/app-layout/app-layout.test.tsx
@@ -1,16 +1,46 @@
 import AppLayout from './app-layout';
 import {render, screen} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
 
-jest.mock('react-redux', () => ({
+/*jest.mock('react-redux', () => ({
   useSelector: jest.fn(),
-}));
+}));*/
 
 describe('test AppLayout FC', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+  const initialState: DeepPartial<StateSchema> = {
+    promo: {isLoaded: false, product: null},
+    searchParams: {
+      sort: '',
+      order: '',
+      minPrice: '',
+      maxPrice: '',
+      levels: [],
+      category: '',
+      groups: [],
+      bannedGroups: []
+    },
+    error: {errors: []},
+    product: {product: null, isProductLoading: false, products: [], isLoading: false, totalPagesCount: 0}
+  };
+  const store = mockStore(initialState);
 
   it('should render without fail', () => {
-    render(<AppLayout/>,
-      {wrapper: BrowserRouter});
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppLayout/>,
+        </BrowserRouter>
+      </Provider>);
     const element = screen.getByTestId('app-layout');
     expect(element).toBeInTheDocument();
   });
