7b0b8ddfc925d5ac06bb835c7402600c12c45606
DenisPepper <thech@yandex.ru>
2023-03-17 17:41:10 +0300

    fixed tests for product-slice reducer

--- src/store/slices/product/slice/product-slice.test.ts
+++ src/store/slices/product/slice/product-slice.test.ts
@@ -1,7 +1,10 @@
 import {productReducer as reducer} from './product-slice';
 import {ProductSchema as StateSchema} from '../schema/product-schema';
 import {stubProduct as product} from '../../../../mocks/stub-product';
-import {fetchProductByIdWithReviews} from '../../../../services/fetch-product-by-id-with-reviews/fetch-product-by-id-with-reviews';
+import {
+  fetchProductByIdWithReviews
+} from '../../../../services/fetch-product-by-id-with-reviews/fetch-product-by-id-with-reviews';
+import {fetchProducts} from '../../../../services/fetch-products/fetch-products';
 
 describe('test of product-slice reducer', () => {
 
@@ -17,23 +20,27 @@ describe('test of product-slice reducer', () => {
       .toEqual(initialState);
   });
 
-  /* it('should update state with new product list', () => {
+  it('should update state with new product list', () => {
     const prevState: Partial<StateSchema> = {
       products: [],
     };
     const updatedState: Partial<StateSchema> = {
       products: [product],
+      isLoading: false,
+      totalPagesCount: 1,
     };
-    expect(reducer(prevState as StateSchema, actions.setProducts([product])))
+    expect(reducer(prevState as StateSchema,
+      {type: fetchProducts.fulfilled.type, payload: {products: [product], totalPagesCount: 1,}}))
       .toEqual(updatedState);
-  });*/
+  });
 
   it('should update state with new product', () => {
     const prevState: Partial<StateSchema> = {
-      product: null
+      product: null,
     };
     const updatedState: Partial<StateSchema> = {
-      product: product
+      product: product,
+      isProductLoading: false,
     };
     expect(reducer(prevState as StateSchema,
       {type: fetchProductByIdWithReviews.fulfilled.type, payload: {product, reviews: [], reviewCount: 0}}))
