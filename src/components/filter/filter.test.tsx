a74b8d2aeb22c4f9dc37adefd53ad32cdcf72634
DenisPepper <thech@yandex.ru>
2023-03-17 17:40:06 +0300

    fixed tests for Filter FC

--- src/components/filter/filter.test.tsx
+++ src/components/filter/filter.test.tsx
@@ -1,10 +1,39 @@
 import Filter from './filter';
 import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
+import {BrowserRouter} from 'react-router-dom';
 
 describe('test Filter', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+  const initialState: DeepPartial<StateSchema> = {
+    searchParams: {
+      sort: '',
+      order: '',
+      minPrice: '',
+      maxPrice: '',
+      levels: [],
+      groups: [],
+      bannedGroups: [],
+      category: '',
+    },
+  };
+  const stubsStore = mockStore(initialState);
 
   it('should render without fail', () => {
-    render(<Filter/>);
+    render(
+      <Provider store={stubsStore}>
+        <BrowserRouter>
+          <Filter/>
+        </BrowserRouter>
+      </Provider>);
     const element = screen.getByRole('heading');
     expect(element).toBeInTheDocument();
   });
