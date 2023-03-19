399b30ba55bf18518a647155198cd7c6c4200512
DenisPepper <thech@yandex.ru>
2023-03-19 20:50:44 +0300

    fixed tests for Header FC

--- src/components/header/header.test.tsx
+++ src/components/header/header.test.tsx
@@ -1,12 +1,29 @@
 import Header from './header';
 import {render, screen} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
 
 describe('test Header FC', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+  const initialState: DeepPartial<StateSchema> = {};
+  const store = mockStore(initialState);
 
   it('should render without fail', () => {
-
-    render(<Header/>, {wrapper: BrowserRouter});
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header/>
+        </BrowserRouter>
+      </Provider>
+    );
     const element = screen.getByTestId('header');
     expect(element).toBeInTheDocument();
   });
