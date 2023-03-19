8b426da350ecdaff09d82f4d2e2eb562c663ddf2
DenisPepper <thech@yandex.ru>
2023-03-19 20:50:25 +0300

    fixed tests for HeaderSearchForm FC

--- src/components/header-search-form/header-search-form.test.tsx
+++ src/components/header-search-form/header-search-form.test.tsx
@@ -1,12 +1,29 @@
 import HeaderSearchForm from './header-search-form';
 import {render, screen} from '@testing-library/react';
 import {BrowserRouter} from 'react-router-dom';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {StateSchema} from '../../store/state-schema';
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import axios from 'axios';
+import {DeepPartial} from '@reduxjs/toolkit';
 
 describe('test HeaderSearchForm FC', () => {
+  const mockStore = configureMockStore<StateSchema,
+    Action,
+    ThunkDispatch<StateSchema, typeof axios, Action>>([thunk]);
+  const initialState: DeepPartial<StateSchema> = {};
+  const store = mockStore(initialState);
 
   it('should render without fail', () => {
-
-    render(<HeaderSearchForm/>, {wrapper: BrowserRouter});
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <HeaderSearchForm/>
+        </BrowserRouter>
+      </Provider>
+    );
     const form = screen.getByTestId(/form-search/);
     expect(form).toBeInTheDocument();
   });
