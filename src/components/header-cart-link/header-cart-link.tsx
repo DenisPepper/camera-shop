6de4b5ffcdb87a72f6830f39cca3cd1e16eed7d4
DenisPepper <thech@yandex.ru>
2023-03-26 21:01:26 +0300

    add cart total count to header cart link

--- src/components/header-cart-link/header-cart-link.tsx
+++ src/components/header-cart-link/header-cart-link.tsx
@@ -1,12 +1,19 @@
 import {Link} from 'react-router-dom';
 import {Path as to} from '../../settings/settings';
+import {shallowEqual, useSelector} from 'react-redux';
+import {getTotalCount} from '../../store/slices/cart/selectors/get-total-count/get-total-count';
 
 export default function HeaderCartLink(): JSX.Element {
-  return(
+  const count = useSelector(getTotalCount, shallowEqual);
+
+  return (
     <Link className="header__basket-link" to={`/${to.Cart}`}>
       <svg width="16" height="16" aria-hidden="true">
         <use xlinkHref="#icon-basket"></use>
       </svg>
+      {
+        !!count && <span className="header__basket-count">{count}</span>
+      }
     </Link>
   );
 }
