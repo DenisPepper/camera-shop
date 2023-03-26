67dfde5cfc5c4356351cf5274570641eb3c1f5ee
DenisPepper <thech@yandex.ru>
2023-03-26 20:37:21 +0300

    modified ProductCardButton FC

--- src/components/product-card-button/product-card-button.tsx
+++ src/components/product-card-button/product-card-button.tsx
@@ -3,6 +3,9 @@ import {DEFAULT_PRODUCT_TAB, Path as to} from '../../settings/settings';
 import React from 'react';
 import CartBuyButton from '../cart-buy-button/cart-buy-button';
 import {ProductType} from '../../types/product-type';
+import {getItems} from '../../store/slices/cart/selectors/get-items/get-items';
+import {shallowEqual, useSelector} from 'react-redux';
+import ProductCartLink from '../product-cart-link/product-cart-link';
 
 interface ProductCardButtonProps {
   product: ProductType;
@@ -10,10 +13,19 @@ interface ProductCardButtonProps {
 
 export default function ProductCardButton(props: ProductCardButtonProps): JSX.Element {
   const {product} = props;
+  const cartItems = useSelector(getItems, shallowEqual);
+  const inCart = cartItems.find((item) => item.id === product.id);
 
   return (
     <div className="product-card__buttons">
-      <CartBuyButton isProductCard product={product}/>
+
+      {
+        inCart ?
+          <ProductCartLink/>
+          :
+          <CartBuyButton isProductCard product={product}/>
+      }
+
       <Link
         className="btn btn--transparent"
         to={`/${to.Product}/${product.id}?tab=${DEFAULT_PRODUCT_TAB}`}
